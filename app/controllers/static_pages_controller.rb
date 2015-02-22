dc042595e82cd69a22c24f37f9d6f7d6f64f2332
angelsystem <angelsystem1387@gmail.com>
2015-02-22 02:55:24 -0500

    two root dependence authenticate

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,4 @@
 class StaticPagesController < ApplicationController
   def home
-    if admin_signed_in?
-      @feed_events = current_admin.events
-    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
 
   devise_for :admins, controllers: { registrations: "admins/registrations" }
+
+  authenticated :admin do
+    root :to => "events#index", as: :authenticated_root
+  end
+
+  unauthenticated :admin do
+    root :to => "static_pages#home"
+  end
+
   resources :clients, only: [:new, :create]
   resources :events do
     resources :participants, only: [:create, :new]
     resources :checklists, only: [:create, :new, :destroy]
   end
   resources :admins, :only => [:show]
-  root :to => "static_pages#home"
 end
