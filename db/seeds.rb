4fb9c1253e1e9123c6b56796ebd0e9a8a0cc594d
videmor <videmor@gmail.com>
2015-03-05 19:04:03 -0500

    categories of companies

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+
+# data category
+
+# end data category
+
+
+
 # 20.times do
 #   Company.create(name: Faker::Company.name, email: Faker::Internet.email, address: Faker::Address.street_address, phone: Faker::PhoneNumber.phone_number, logo: "http://pigment.github.io/fake-logos/logos/small/color/#{rand(1...13)}.png")
 # end
@@ -14,17 +21,13 @@
 # Admin.create email: 'angelsystem@gmail.com', fullname: 'angel rojitas', company_id: Company.pluck(:id).sample, password: 'please'
 # Admin.create email: 'gramirez@gmail.com', fullname: 'chalo gonzale', company_id: Company.pluck(:id).sample, password: 'please'
 
-20.times do
-  admin = Admin.all.sample
-  Client.create(fullname: Faker::Name.name, dni: Faker::Address.building_number, email: Faker::Internet.email, company_id: admin.company, creator_id: admin.id, gender: ['f', 'm'].sample)
-end
-
-20.times do
-  latitude = rand(-90.000000000...90.000000000)
-  longitude = rand(-180.000000000...180.000000000)
-  Event.create(name: Faker::Lorem.word, start_at: Time.now + rand((1..30)).days, duration: rand(1..10), place: Faker::Company.name, location: Faker::Address.street_address, latitude: latitude, longitude: longitude,  address: Faker::Address.city, company_id: Company.pluck(:id).sample, creator_id: Admin.pluck(:id).sample, description: Faker::Lorem.sentence(3))
-end
-
-10.times do
-  Category.create(name: Faker::Commerce.product_name)
-end
+# 20.times do
+#   admin = Admin.all.sample
+#   Client.create(fullname: Faker::Name.name, dni: Faker::Address.building_number, email: Faker::Internet.email, company_id: admin.company, creator_id: admin.id, gender: ['f', 'm'].sample)
+# end
+#
+# 20.times do
+#   latitude = rand(-90.000000000...90.000000000)
+#   longitude = rand(-180.000000000...180.000000000)
+#   Event.create(name: Faker::Lorem.word, start_at: Time.now + rand((1..30)).days, duration: rand(1..10), place: Faker::Company.name, location: Faker::Address.street_address, latitude: latitude, longitude: longitude,  address: Faker::Address.city, company_id: Company.pluck(:id).sample, creator_id: Admin.pluck(:id).sample, description: Faker::Lorem.sentence(3))
+# end
