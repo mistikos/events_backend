385846e6070804a594c7e9001b652d1b2d9519c2
videmor <videmor@gmail.com>
2015-03-05 19:07:09 -0500

    advanced

--- db/migrate/20150221032923_create_companies.rb
+++ db/migrate/20150221032923_create_companies.rb
@@ -1,11 +1,12 @@
 class CreateCompanies < ActiveRecord::Migration
   def change
     create_table :companies do |t|
-      t.string :name
-      t.string :email
+      t.string :name, null: false
+      t.string :email, null: false
       t.string :address
       t.string :phone
       t.string :logo
+      # t.belongs_to :categor
 
       t.timestamps
     end
