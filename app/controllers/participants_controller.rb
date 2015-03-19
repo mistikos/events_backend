2fd87c2dc3129f33abc00227dd40b5e453f906fa
angelsystem <angelsystem1387@gmail.com>
2015-03-19 17:36:45 -0500

    event details

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,25 +1,37 @@
 class ParticipantsController < ApplicationController
+  before_action :set_event, only: [:new, :create]
+
   def index
   end
 
   def new
-    @event = Event.new(start_at: Time.zone.now)
+    respond_to do |format|
+      format.js
+    end
   end
 
   def create
-    @event = Event.find(params[:event_id])
     @participant = @event.participants.build(params_participant)
-    if @participant.save
-      flash[:success] = "Participante agregado!"
+    if @participant.email.present?
       redirect_to @event
+      flash[:success] = "Se envió correctament el correo"
     else
-      flash[:alert] = "Ocurrio un error!"
-      redirect_to @event
+      if @participant.save
+        flash[:success] = "Participante agregado!"
+        redirect_to @event
+      else
+        flash[:error] = "Ocurrio un error!"
+        redirect_to @event
+      end
     end
   end
 
   private
   def params_participant
-    params.require(:participant).permit(:company_id)
+    params.require(:participant).permit(:company_id, :email)
+  end
+
+  def set_event
+    @event = Event.find(params[:event_id])
   end
 end
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,8 +1,17 @@
 class Participant < ActiveRecord::Base
 
+  attr_accessor :email
+
   belongs_to :event
   belongs_to :company
 
+  validates :event, presence: true, if: :send_email?
+  validates :company, presence: true, if: :send_email?
+
   validates :company, uniqueness: { scope: :event, message: "Ya elegiste la empresa" }
+
+  def send_email?
+    email == ""
+  end
 
 end
